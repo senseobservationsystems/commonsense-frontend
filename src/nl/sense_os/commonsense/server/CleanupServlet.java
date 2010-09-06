773e2e2aeb36c46d26423b7ea6492ac8703aa88a
Andries Stam <andries@almende.com>
2010-09-06 11:06:49 +0000

    changed the cleanupservlet

--- src/nl/sense_os/commonsense/server/CleanupServlet.java
+++ src/nl/sense_os/commonsense/server/CleanupServlet.java
@@ -4,17 +4,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.jdo.JDOFatalUserException;
 import javax.jdo.PersistenceManager;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import nl.sense_os.commonsense.server.data.BooleanValue;
-import nl.sense_os.commonsense.server.data.FloatValue;
-import nl.sense_os.commonsense.server.data.JsonValue;
 import nl.sense_os.commonsense.server.data.PMF;
 import nl.sense_os.commonsense.server.data.SensorType;
-import nl.sense_os.commonsense.server.data.StringValue;
 
 @SuppressWarnings("serial")
 public class CleanupServlet extends HttpServlet {
@@ -25,32 +22,20 @@ public class CleanupServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws IOException {
 		resp.setContentType("text/plain");
-
+		
+		String entityName = req.getParameter("entityName");
+		
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		try {
-			String query = "select from " + SensorType.class.getName();
-			List<SensorType> sensorTypes = (List<SensorType>) pm.newQuery(query).execute();
-			pm.deletePersistentAll(sensorTypes);
-
-			query = "select from " + FloatValue.class.getName();
-			List<FloatValue> floatValues = (List<FloatValue>) pm.newQuery(query).execute();
-			pm.deletePersistentAll(floatValues);
-
-			query = "select from " + StringValue.class.getName();
-			List<StringValue> stringValues = (List<StringValue>) pm.newQuery(query).execute();
-			pm.deletePersistentAll(stringValues);
-
-			query = "select from " + JsonValue.class.getName();
-			List<JsonValue> jsonValues = (List<JsonValue>) pm.newQuery(query).execute();
-			pm.deletePersistentAll(jsonValues);
-
-			query = "select from " + BooleanValue.class.getName();
-			List<BooleanValue> booleanValues = (List<BooleanValue>) pm.newQuery(query).execute();
-			pm.deletePersistentAll(booleanValues);
-			
+			String query = "select from " + entityName;
+			List<Object> objects = (List<Object>) pm.newQuery(query).execute();
+			pm.deletePersistentAll(objects);
+		} catch (JDOFatalUserException e) {
+			resp.getWriter().println("Problem: " + entityName + " unknown!");
+			log.warning("Cleanup failed - entityName " + entityName + " unknown.");
 		} finally {
 			pm.close();
-		}
+		} 
 		resp.getWriter().println("Everything removed!");
 	}	
 }
