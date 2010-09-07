19d0ef65ab5157934e9a56ef3277931fb5daab9b
Andries Stam <andries@almende.com>
2010-09-07 13:09:25 +0000

    updated the cleanup servlet

--- src/nl/sense_os/commonsense/server/CleanupServlet.java
+++ src/nl/sense_os/commonsense/server/CleanupServlet.java
@@ -12,30 +12,44 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import nl.sense_os.commonsense.server.data.PMF;
-import nl.sense_os.commonsense.server.data.SensorType;
 
 @SuppressWarnings("serial")
 public class CleanupServlet extends HttpServlet {
 
 	protected static final Logger log = Logger.getLogger("IVOServlet");
 
+	@SuppressWarnings("unchecked")
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws IOException {
 		resp.setContentType("text/plain");
 		
 		String entityName = req.getParameter("entityName");
-		
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		try {
-			String query = "select from " + entityName;
-			Query q = pm.newQuery(query);
-			long deletedCount = q.deletePersistentAll();
-			resp.getWriter().println(deletedCount + " Entities removed!");
-		} catch (JDOFatalUserException e) {
-			resp.getWriter().println("Problem: " + entityName + " unknown!");
-			log.warning("Cleanup failed - entityName " + entityName + " unknown.");
-		} finally {
-			pm.close();
-		} 
+		if (entityName == null)
+			resp.getWriter().println("Specify entityName and amount (optional)");
+		else {
+			int amount;
+			String amountString = req.getParameter("amount");
+			if (amountString != null)
+				amount = Integer.parseInt(amountString);
+			else
+				amount = -1;
+			
+			
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			try {
+				String query = "select from " + entityName;
+				Query q = pm.newQuery(query);
+				if (amount != -1)
+					q.setRange(0, amount);
+				List<Object> o = (List<Object>) q.execute();
+				pm.deletePersistentAll(o);
+				resp.getWriter().println(o.size());
+			} catch (JDOFatalUserException e) {
+				resp.getWriter().println("Problem: " + entityName + " unknown!");
+				log.warning("Cleanup failed - entityName " + entityName + " unknown.");
+			} finally {
+				pm.close();
+			} 
+		}
 	}	
 }
---
+++ src/nl/sense_os/commonsense/tools/GlobalCleanup.java
@@ -0,0 +1,55 @@
+package nl.sense_os.commonsense.tools;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import nl.sense_os.commonsense.server.data.BooleanValue;
+import nl.sense_os.commonsense.server.data.FloatValue;
+import nl.sense_os.commonsense.server.data.JsonValue;
+import nl.sense_os.commonsense.server.data.SensorType;
+import nl.sense_os.commonsense.server.data.StringValue;
+
+public class GlobalCleanup {
+
+	private static String URL = "http://common-sense-test.appspot.com/ivo/cleanup";
+	private static String entityNameVar = "?entityName=";
+	private static String amountVar = "&amount=";
+	private static String basicAmount = "100";
+
+	private static void removeAll(String type) {
+		try {
+			URL url;
+			url = new URL(URL + entityNameVar + type + amountVar + basicAmount);
+
+   			int amount = -1;
+   			int total = 0;
+   			while (amount != 0) {
+   				BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+   				amount = Integer.parseInt(in.readLine());
+   				total = total + amount;
+   				System.out.print(".");
+   				in.close();
+			}
+   			System.out.println();
+   			System.out.println(total + " entities of type " + type + " removed.");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	    
+	public static void main(String[] args) {
+		removeAll(SensorType.class.getName());
+		removeAll(FloatValue.class.getName());
+		removeAll(BooleanValue.class.getName());
+		removeAll(JsonValue.class.getName());
+		removeAll(StringValue.class.getName());
+	}
+
+	
+
+}
