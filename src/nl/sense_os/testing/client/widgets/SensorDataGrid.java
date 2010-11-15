5f72cb19d9a0687bda16f546e67b2954e3a538c2
Federico Hernandez <federico.hernandez@rotterdam-cs.com>
2010-11-15 15:17:25 +0000

    URL modified.

--- src/nl/sense_os/testing/client/widgets/SensorDataGrid.java
+++ src/nl/sense_os/testing/client/widgets/SensorDataGrid.java
@@ -74,8 +74,8 @@ public class SensorDataGrid extends LayoutContainer {
 		
 		// Grid.
 		PaginationGridPanel gridPanel = new PaginationGridPanel(
-				"http://dev2.almende.com/commonsense/get_sensor_data_paged.php" +
-				"?email=steven@sense-os.nl&password=81dc9bdb52d04dc20036dbd8313ed055&d_id=78&s_id=2",
+				"http://dev2.almende.com/commonsense/testing/get_sensor_data_paged.php" +
+				"?email=steven@sense-os.nl&password=81dc9bdb52d04dc20036dbd8313ed055&s_id=2",
 				model,
 				cm,
 				5);
