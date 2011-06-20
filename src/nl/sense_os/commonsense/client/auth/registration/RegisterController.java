f491fb11d12cfc69da4181ed7a7686cf94817eb9
Steven Mulder <steven@sense-os.nl>
2011-06-20 14:00:45 +0000

    Changed registration code to use regular RequestBuilder instead of the old complicated XHR JSNI solution.

--- src/nl/sense_os/commonsense/client/auth/registration/RegisterController.java
+++ src/nl/sense_os/commonsense/client/auth/registration/RegisterController.java
@@ -12,6 +12,11 @@ import com.extjs.gxt.ui.client.mvc.AppEvent;
 import com.extjs.gxt.ui.client.mvc.Controller;
 import com.extjs.gxt.ui.client.mvc.Dispatcher;
 import com.extjs.gxt.ui.client.mvc.View;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
 
 public class RegisterController extends Controller {
 
@@ -74,7 +79,56 @@ public class RegisterController extends Controller {
         forwardToView(form, new AppEvent(RegisterEvents.RegisterSuccess));
     }
 
-    private void register(String username, String password, String name, String surname,
+    private void register(final String username, final String password, String name,
+            String surname, String email, String mobile) {
+
+        // prepare request properties
+        final String url = Urls.USERS + ".json";
+
+        // create request body
+        String userJson = "\"user\":{";
+        userJson += "\"username\":\"" + username + "\"";
+        userJson += "," + "\"password\":\"" + Md5Hasher.hash(password) + "\"";
+        userJson += "," + "\"name\":\"" + name + "\"";
+        userJson += "," + "\"surname\":\"" + surname + "\"";
+        userJson += "," + "\"email\":\"" + email + "\"";
+        userJson += "," + "\"mobile\":\"" + mobile + "\"";
+        userJson += "}";
+        final String body = "{" + userJson + "}";
+
+        // prepare request callback
+        RequestCallback callback = new RequestCallback() {
+
+            @Override
+            public void onError(Request request, Throwable exception) {
+                LOG.warning("POST registration onError callback: " + exception.getMessage());
+                onRegisterFailure();
+            }
+
+            @Override
+            public void onResponseReceived(Request request, Response response) {
+                LOG.finest("POST registration response received: " + response.getStatusText());
+                int statusCode = response.getStatusCode();
+                if (201 == statusCode) {
+                    onRegisterSuccess(response.getText(), username, password);
+                } else {
+                    LOG.warning("POST registration returned incorrect status: " + statusCode);
+                    onRegisterFailure();
+                }
+            }
+        };
+
+        // send request
+        RequestBuilder builder = new RequestBuilder(RequestBuilder.POST, url);
+        try {
+            builder.sendRequest(body, callback);
+        } catch (RequestException e) {
+            LOG.warning("POST registration request threw exception: " + e.getMessage());
+            onRegisterFailure();
+        }
+    }
+
+    private void xhrRegister(String username, String password, String name, String surname,
             String email, String mobile) {
 
         String hashPass = Md5Hasher.hash(password);
