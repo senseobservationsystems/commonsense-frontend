b461b24e6ef706abb4a2b0c90c337de45f45412e
Steven Mulder <steven@sense-os.nl>
2011-07-22 07:56:04 +0000

    add filtering to state creator grid

--- src/nl/sense_os/commonsense/client/states/create/StateCreator.java
+++ src/nl/sense_os/commonsense/client/states/create/StateCreator.java
@@ -5,14 +5,17 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import nl.sense_os.commonsense.client.common.components.CenteredWindow;
+import nl.sense_os.commonsense.client.common.constants.Constants;
 import nl.sense_os.commonsense.client.common.models.SensorModel;
 import nl.sense_os.commonsense.client.common.models.ServiceModel;
+import nl.sense_os.commonsense.client.common.models.UserModel;
 import nl.sense_os.commonsense.client.sensors.library.LibraryColumnsFactory;
 import nl.sense_os.commonsense.client.sensors.library.SensorGroupRenderer;
 import nl.sense_os.commonsense.client.utility.SenseIconProvider;
 import nl.sense_os.commonsense.client.utility.SenseKeyProvider;
 import nl.sense_os.commonsense.client.utility.SensorProcessor;
 
+import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.Style.HorizontalAlignment;
 import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.data.BaseModelData;
@@ -20,6 +23,8 @@ import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.TreeModel;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.EventType;
+import com.extjs.gxt.ui.client.event.Events;
+import com.extjs.gxt.ui.client.event.FieldEvent;
 import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.event.SelectionChangedEvent;
@@ -30,15 +35,19 @@ import com.extjs.gxt.ui.client.mvc.Controller;
 import com.extjs.gxt.ui.client.mvc.View;
 import com.extjs.gxt.ui.client.store.GroupingStore;
 import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.store.Store;
+import com.extjs.gxt.ui.client.store.StoreFilter;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.MessageBox;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.AdapterField;
+import com.extjs.gxt.ui.client.widget.form.CheckBox;
 import com.extjs.gxt.ui.client.widget.form.ComboBox;
 import com.extjs.gxt.ui.client.widget.form.ComboBox.TriggerAction;
 import com.extjs.gxt.ui.client.widget.form.FormButtonBinding;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
+import com.extjs.gxt.ui.client.widget.form.StoreFilterField;
 import com.extjs.gxt.ui.client.widget.form.TextField;
 import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
@@ -46,6 +55,9 @@ import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.grid.GroupingView;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.FormData;
+import com.extjs.gxt.ui.client.widget.toolbar.LabelToolItem;
+import com.extjs.gxt.ui.client.widget.toolbar.SeparatorToolItem;
+import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 
 public class StateCreator extends View {
 
@@ -58,6 +70,8 @@ public class StateCreator extends View {
     private AdapterField sensorsField;
     private GroupingStore<SensorModel> sensorsStore;
     private Grid<SensorModel> sensorsGrid;
+    private ToolBar sensorsFilterBar;
+    private StoreFilterField<SensorModel> sensorsFilter;
     private ListStore<ModelData> dataFieldsStore;
     private Grid<ModelData> dataFieldsGrid;
     private AdapterField dataFieldsField;
@@ -128,36 +142,38 @@ public class StateCreator extends View {
                 }
             }
         };
-        this.createButton = new Button("Create", SenseIconProvider.ICON_BUTTON_GO, l);
+        createButton = new Button("Create", SenseIconProvider.ICON_BUTTON_GO, l);
 
-        this.cancelButton = new Button("Cancel", l);
+        cancelButton = new Button("Cancel", l);
 
-        final FormButtonBinding binding = new FormButtonBinding(this.form);
-        binding.addButton(this.createButton);
+        final FormButtonBinding binding = new FormButtonBinding(form);
+        binding.addButton(createButton);
 
-        this.form.setButtonAlign(HorizontalAlignment.CENTER);
-        this.form.addButton(this.createButton);
-        this.form.addButton(this.cancelButton);
+        form.setButtonAlign(HorizontalAlignment.CENTER);
+        form.addButton(createButton);
+        form.addButton(cancelButton);
     }
 
     private void initFields() {
 
-        this.nameField = new TextField<String>();
-        this.nameField.setFieldLabel("State sensor name");
-        this.nameField.setAllowBlank(false);
+        nameField = new TextField<String>();
+        nameField.setFieldLabel("State sensor name");
+        nameField.setAllowBlank(false);
 
-        initSensorsTree();
+        initSensorsGrid();
+        initSensorsFilter();
         ContentPanel sensorsPanel = new ContentPanel(new FitLayout());
         sensorsPanel.setHeaderVisible(false);
         sensorsPanel.setStyleAttribute("backgroundColor", "white");
-        sensorsPanel.add(this.sensorsGrid);
+        sensorsPanel.setTopComponent(sensorsFilterBar);
+        sensorsPanel.add(sensorsGrid);
 
-        this.sensorsField = new AdapterField(sensorsPanel);
-        this.sensorsField.setHeight(300);
-        this.sensorsField.setResizeWidget(true);
-        this.sensorsField.setFieldLabel("Input sensor");
+        sensorsField = new AdapterField(sensorsPanel);
+        sensorsField.setHeight(300);
+        sensorsField.setResizeWidget(true);
+        sensorsField.setFieldLabel("Input sensor");
 
-        this.sensorsGrid.getSelectionModel().addSelectionChangedListener(
+        sensorsGrid.getSelectionModel().addSelectionChangedListener(
                 new SelectionChangedListener<SensorModel>() {
 
                     @Override
@@ -176,97 +192,148 @@ public class StateCreator extends View {
                     }
                 });
 
-        this.servicesStore = new ListStore<ServiceModel>();
+        servicesStore = new ListStore<ServiceModel>();
 
-        this.servicesField = new ComboBox<ServiceModel>();
-        this.servicesField.setFieldLabel("Algorithm type");
-        this.servicesField.setEmptyText("Select service algorithm type...");
-        this.servicesField.setStore(this.servicesStore);
-        this.servicesField.setDisplayField(ServiceModel.NAME);
-        this.servicesField.setAllowBlank(false);
-        this.servicesField.setTriggerAction(TriggerAction.ALL);
-        this.servicesField.setTypeAhead(true);
-        this.servicesField.setForceSelection(true);
+        servicesField = new ComboBox<ServiceModel>();
+        servicesField.setFieldLabel("Algorithm type");
+        servicesField.setEmptyText("Select service algorithm type...");
+        servicesField.setStore(servicesStore);
+        servicesField.setDisplayField(ServiceModel.NAME);
+        servicesField.setAllowBlank(false);
+        servicesField.setTriggerAction(TriggerAction.ALL);
+        servicesField.setTypeAhead(true);
+        servicesField.setForceSelection(true);
 
         // update sensors and data fields when a service is selected
-        this.servicesField
-                .addSelectionChangedListener(new SelectionChangedListener<ServiceModel>() {
+        servicesField.addSelectionChangedListener(new SelectionChangedListener<ServiceModel>() {
 
-                    @Override
-                    public void selectionChanged(SelectionChangedEvent<ServiceModel> se) {
-                        ServiceModel selected = se.getSelectedItem();
-                        SensorModel sensor = sensorsGrid.getSelectionModel().getSelectedItem();
-                        dataFieldsStore.removeAll();
+            @Override
+            public void selectionChanged(SelectionChangedEvent<ServiceModel> se) {
+                ServiceModel selected = se.getSelectedItem();
+                SensorModel sensor = sensorsGrid.getSelectionModel().getSelectedItem();
+                dataFieldsStore.removeAll();
 
-                        if (null != selected) {
-                            String sensorName = sensor.<String> get(SensorModel.NAME);
-                            sensorName = sensorName.replace(' ', '_');
+                if (null != selected) {
+                    String sensorName = sensor.<String> get(SensorModel.NAME);
+                    sensorName = sensorName.replace(' ', '_');
 
-                            List<String> dataFields = selected
-                                    .<List<String>> get(ServiceModel.DATA_FIELDS);
-                            for (String fieldName : dataFields) {
-                                if (fieldName.contains(sensorName)
-                                        && fieldName.length() > sensorName.length()) {
-                                    int beginIndex = sensorName.length() + 1;
-                                    fieldName = fieldName.substring(beginIndex, fieldName.length());
-                                }
-                                ModelData fieldModel = new BaseModelData();
-                                fieldModel.set("text", fieldName);
-                                dataFieldsStore.add(fieldModel);
-                            }
+                    List<String> dataFields = selected.<List<String>> get(ServiceModel.DATA_FIELDS);
+                    for (String fieldName : dataFields) {
+                        if (fieldName.contains(sensorName)
+                                && fieldName.length() > sensorName.length()) {
+                            int beginIndex = sensorName.length() + 1;
+                            fieldName = fieldName.substring(beginIndex, fieldName.length());
                         }
+                        ModelData fieldModel = new BaseModelData();
+                        fieldModel.set("text", fieldName);
+                        dataFieldsStore.add(fieldModel);
                     }
-                });
+                }
+            }
+        });
 
-        this.dataFieldsStore = new ListStore<ModelData>();
+        dataFieldsStore = new ListStore<ModelData>();
 
-        ColumnModel cm = new ColumnModel(Arrays.asList(new ColumnConfig("text", "", this.form
+        ColumnModel cm = new ColumnModel(Arrays.asList(new ColumnConfig("text", "", form
                 .getFieldWidth())));
-        dataFieldsGrid = new Grid<ModelData>(this.dataFieldsStore, cm);
+        dataFieldsGrid = new Grid<ModelData>(dataFieldsStore, cm);
         dataFieldsGrid.setHideHeaders(true);
         dataFieldsGrid.setAutoExpandColumn("text");
 
-        this.dataFieldsField = new AdapterField(dataFieldsGrid);
-        this.dataFieldsField.setFieldLabel("Data fields");
-        this.dataFieldsField.setResizeWidget(true);
-        this.dataFieldsField.setBorders(true);
+        dataFieldsField = new AdapterField(dataFieldsGrid);
+        dataFieldsField.setFieldLabel("Data fields");
+        dataFieldsField.setResizeWidget(true);
+        dataFieldsField.setBorders(true);
 
         final FormData formData = new FormData("-10");
-        this.form.add(this.nameField, formData);
-        this.form.add(this.sensorsField, formData);
-        this.form.add(this.servicesField, formData);
-        this.form.add(this.dataFieldsField, formData);
+        form.add(nameField, formData);
+        form.add(sensorsField, formData);
+        form.add(servicesField, formData);
+        form.add(dataFieldsField, formData);
     }
 
     private void initForm() {
 
-        this.form = new FormPanel();
-        this.form.setHeaderVisible(false);
-        this.form.setBodyBorder(false);
-        this.form.setScrollMode(Scroll.AUTOY);
+        form = new FormPanel();
+        form.setHeaderVisible(false);
+        form.setBodyBorder(false);
+        form.setScrollMode(Scroll.AUTOY);
+        form.setLabelWidth(150);
 
         initFields();
         initButtons();
 
-        this.window.add(form);
+        window.add(form);
     }
 
     @Override
     protected void initialize() {
         super.initialize();
 
-        this.window = new CenteredWindow();
-        this.window.setHeading("Create state sensor");
-        this.window.setSize(450, 550);
-        this.window.setLayout(new FitLayout());
+        window = new CenteredWindow();
+        window.setHeading("Create state sensor");
+        window.setSize(720, 550);
+        window.setLayout(new FitLayout());
 
         initForm();
     }
 
-    private void initSensorsTree() {
+    private void initSensorsFilter() {
 
-        this.sensorsStore = new GroupingStore<SensorModel>();
-        this.sensorsStore.setKeyProvider(new SenseKeyProvider<SensorModel>());
+        sensorsFilterBar = new ToolBar();
+        sensorsFilterBar.add(new LabelToolItem("Filter: "));
+        sensorsFilter = new StoreFilterField<SensorModel>() {
+
+            @Override
+            protected boolean doSelect(Store<SensorModel> store, SensorModel parent,
+                    SensorModel record, String property, String filter) {
+
+                String matchMe = record.getDisplayName().toLowerCase() + " "
+                        + record.getPhysicalSensor().toLowerCase() + " "
+                        + record.<String> get(SensorModel.DEVICE_TYPE, "").toLowerCase() + " "
+                        + record.<String> get(SensorModel.ENVIRONMENT_NAME, "").toLowerCase();
+                if (matchMe.contains(filter.toLowerCase())) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        final CheckBox onlyMe = new CheckBox();
+        onlyMe.setBoxLabel("Only my own sensors");
+        onlyMe.setHideLabel(true);
+        onlyMe.addListener(Events.Change, new Listener<FieldEvent>() {
+
+            @Override
+            public void handleEvent(FieldEvent be) {
+                StoreFilter<SensorModel> filter = new StoreFilter<SensorModel>() {
+                    @Override
+                    public boolean select(com.extjs.gxt.ui.client.store.Store<SensorModel> store,
+                            SensorModel parent, SensorModel item, String property) {
+                        UserModel user = Registry.get(Constants.REG_USER);
+                        return item.get(SensorModel.OWNER_USERNAME, "").equals(user.getUsername());
+                    };
+                };
+
+                if (onlyMe.getValue()) {
+                    sensorsStore.addFilter(filter);
+                    sensorsStore.applyFilters(null);
+                } else {
+                    sensorsStore.removeFilter(filter);
+                    sensorsStore.clearFilters();
+                }
+            }
+        });
+        sensorsFilter.bind(sensorsStore);
+        sensorsFilterBar.add(sensorsFilter);
+        sensorsFilterBar.add(new SeparatorToolItem());
+        sensorsFilterBar.add(onlyMe);
+    }
+
+    private void initSensorsGrid() {
+
+        sensorsStore = new GroupingStore<SensorModel>();
+        sensorsStore.setKeyProvider(new SenseKeyProvider<SensorModel>());
 
         // Column model
         ColumnModel cm = LibraryColumnsFactory.create();
@@ -277,31 +344,31 @@ public class StateCreator extends View {
         view.setForceFit(true);
         view.setGroupRenderer(new SensorGroupRenderer(cm));
 
-        this.sensorsGrid = new Grid<SensorModel>(this.sensorsStore, cm);
-        this.sensorsGrid.setModelProcessor(new SensorProcessor<SensorModel>());
-        this.sensorsGrid.setView(view);
-        this.sensorsGrid.setBorders(false);
+        sensorsGrid = new Grid<SensorModel>(sensorsStore, cm);
+        sensorsGrid.setModelProcessor(new SensorProcessor<SensorModel>());
+        sensorsGrid.setView(view);
+        sensorsGrid.setBorders(false);
     }
 
     private void onAvailableServicesComplete(List<ServiceModel> services) {
-        this.servicesStore.removeAll();
-        this.dataFieldsStore.removeAll();
+        servicesStore.removeAll();
+        dataFieldsStore.removeAll();
 
         if (services != null) {
-            this.servicesStore.add(services);
+            servicesStore.add(services);
         } else {
-            this.window.hide();
+            window.hide();
             MessageBox.alert(null, "Error getting list of available services!", null);
         }
     }
 
     private void onCancelled(AppEvent event) {
-        this.window.hide();
+        window.hide();
         setBusy(false);
     }
 
     private void onComplete(AppEvent event) {
-        this.window.hide();
+        window.hide();
         setBusy(false);
     }
 
@@ -322,32 +389,32 @@ public class StateCreator extends View {
     }
 
     private void onLoadSensorsComplete(List<SensorModel> sensors) {
-        this.sensorsStore.removeAll();
-        this.servicesStore.removeAll();
-        this.dataFieldsStore.removeAll();
+        sensorsStore.removeAll();
+        servicesStore.removeAll();
+        dataFieldsStore.removeAll();
 
         if (sensors != null) {
-            this.sensorsStore.add(sensors);
+            sensorsStore.add(sensors);
         } else {
-            this.window.hide();
+            window.hide();
             MessageBox.alert(null, "Error getting list of source sensors!", null);
         }
     }
 
     private void setBusy(boolean busy) {
         if (busy) {
-            this.createButton.setIcon(SenseIconProvider.ICON_LOADING);
-            this.cancelButton.disable();
+            createButton.setIcon(SenseIconProvider.ICON_LOADING);
+            cancelButton.disable();
         } else {
-            this.createButton.setIcon(SenseIconProvider.ICON_BUTTON_GO);
-            this.cancelButton.enable();
+            createButton.setIcon(SenseIconProvider.ICON_BUTTON_GO);
+            cancelButton.enable();
         }
     }
 
     private void showWindow() {
-        this.form.reset();
-        this.window.show();
-        this.window.center();
+        form.reset();
+        window.show();
+        window.center();
 
         fireEvent(StateCreateEvents.LoadSensors);
     }
@@ -356,10 +423,10 @@ public class StateCreator extends View {
         setBusy(true);
 
         AppEvent event = new AppEvent(StateCreateEvents.CreateServiceRequested);
-        event.setData("name", this.nameField.getValue());
-        event.setData("service", this.servicesField.getValue());
-        event.setData("sensor", this.sensorsGrid.getSelectionModel().getSelectedItem());
-        event.setData("dataFields", this.dataFieldsGrid.getSelectionModel().getSelectedItems());
+        event.setData("name", nameField.getValue());
+        event.setData("service", servicesField.getValue());
+        event.setData("sensor", sensorsGrid.getSelectionModel().getSelectedItem());
+        event.setData("dataFields", dataFieldsGrid.getSelectionModel().getSelectedItems());
         fireEvent(event);
     }
 }
