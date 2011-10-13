af056bfa2e56555afd990412dfd8b43b1c727aa2
Steven Mulder <steven@sense-os.nl>
2011-10-13 15:47:11 +0000

    deployed new version

--- src/nl/sense_os/commonsense/client/LastDeployed.java
+++ src/nl/sense_os/commonsense/client/LastDeployed.java
@@ -2,7 +2,7 @@ package nl.sense_os.commonsense.client;
 
 public class LastDeployed {
 
-    private static final String deployed = "Thu Oct 13 17:34";
+    private static final String deployed = "Thu Oct 13 17:42";
 
     private LastDeployed() {
         // do not instantiate
