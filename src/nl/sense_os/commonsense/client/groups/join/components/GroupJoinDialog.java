25059052f07eddca8cceeb7c6991073dd37bfd0d
Steven Mulder <steven@sense-os.nl>
2012-07-13 13:00:29 +0000

    fixed bug in table visualization

--- src/nl/sense_os/commonsense/client/groups/join/components/GroupJoinDialog.java
+++ src/nl/sense_os/commonsense/client/groups/join/components/GroupJoinDialog.java
@@ -2,7 +2,6 @@ package nl.sense_os.commonsense.client.groups.join.components;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import nl.sense_os.commonsense.client.common.components.CenteredWindow;
@@ -17,220 +16,218 @@ import com.extjs.gxt.ui.client.widget.layout.CardLayout;
 
 public class GroupJoinDialog extends CenteredWindow {
 
-    public class States {
-        public static final int GROUP_TYPE_CHOICE = 0;
-        public static final int ALL_VISIBLE_GROUPS = 1;
-        public static final int GROUP_NAME = 2;
-        public static final int SHARE_SENSORS = 3;
-        public static final int MEMBER_RIGHTS = 4;
-    }
+	public class States {
+		public static final int GROUP_TYPE_CHOICE = 0;
+		public static final int ALL_VISIBLE_GROUPS = 1;
+		public static final int GROUP_NAME = 2;
+		public static final int SHARE_SENSORS = 3;
+		public static final int MEMBER_RIGHTS = 4;
+	}
 
-    private static final Logger LOG = Logger.getLogger(GroupJoinDialog.class.getName());
+	private static final Logger LOG = Logger.getLogger(GroupJoinDialog.class.getName());
 
-    private CardLayout layout;
-    private GroupTypeForm frmGroupType;
-    private AllVisibleGroupsForm frmAllVisibleGroups;
-    private GroupNameForm frmGroupName;
-    private ShareSensorsForm frmShareSensors;
-    private MemberRightsForm frmMemberRights;
-    private Button btnNext;
-    private Button btnBack;
-    private Button btnCancel;
-    private FormButtonBinding buttonBinding;
-    private int state;
+	private CardLayout layout;
+	private GroupTypeForm frmGroupType;
+	private AllVisibleGroupsForm frmAllVisibleGroups;
+	private GroupNameForm frmGroupName;
+	private ShareSensorsForm frmShareSensors;
+	private MemberRightsForm frmMemberRights;
+	private Button btnNext;
+	private Button btnBack;
+	private Button btnCancel;
+	private FormButtonBinding buttonBinding;
+	private int state;
 
-    public GroupJoinDialog(PagingLoader<PagingLoadResult<GroupModel>> groupLoader,
-            List<SensorModel> sensors) {
-        super();
-        LOG.setLevel(Level.ALL);
+	public GroupJoinDialog(PagingLoader<PagingLoadResult<GroupModel>> groupLoader,
+			List<SensorModel> sensors) {
 
-        // main window properties
-        setHeading("Join a public group");
-        setClosable(false);
-        setSize(540, 480);
+		// main window properties
+		setHeading("Join a public group");
+		setClosable(false);
+		setSize(540, 480);
 
-        // set card layout
-        layout = new CardLayout();
-        setLayout(layout);
+		// set card layout
+		layout = new CardLayout();
+		setLayout(layout);
 
-        // init individual subforms
-        frmGroupType = new GroupTypeForm();
-        add(frmGroupType);
-        frmAllVisibleGroups = new AllVisibleGroupsForm(groupLoader);
-        add(frmAllVisibleGroups);
-        frmGroupName = new GroupNameForm();
-        add(frmGroupName);
-        frmShareSensors = new ShareSensorsForm(new ArrayList<String>(), new ArrayList<String>(),
-                sensors);
-        add(frmShareSensors);
-        frmMemberRights = new MemberRightsForm();
-        add(frmMemberRights);
+		// init individual subforms
+		frmGroupType = new GroupTypeForm();
+		add(frmGroupType);
+		frmAllVisibleGroups = new AllVisibleGroupsForm(groupLoader);
+		add(frmAllVisibleGroups);
+		frmGroupName = new GroupNameForm();
+		add(frmGroupName);
+		frmShareSensors = new ShareSensorsForm(new ArrayList<String>(), new ArrayList<String>(),
+				sensors);
+		add(frmShareSensors);
+		frmMemberRights = new MemberRightsForm();
+		add(frmMemberRights);
 
-        initButtons();
+		initButtons();
 
-        // initialize
-        setWizardState(States.GROUP_TYPE_CHOICE);
-    }
+		// initialize
+		setWizardState(States.GROUP_TYPE_CHOICE);
+	}
 
-    public Button getBtnBack() {
-        return btnBack;
-    }
+	public Button getBtnBack() {
+		return btnBack;
+	}
 
-    public Button getBtnCancel() {
-        return btnCancel;
-    }
+	public Button getBtnCancel() {
+		return btnCancel;
+	}
 
-    public Button getBtnSubmit() {
-        return btnNext;
-    }
+	public Button getBtnSubmit() {
+		return btnNext;
+	}
 
-    public GroupModel getGroup() {
-        return frmAllVisibleGroups.getGroup();
-    }
+	public GroupModel getGroup() {
+		return frmAllVisibleGroups.getGroup();
+	}
 
-    public String getGroupType() {
-        return frmGroupType.getType();
-    }
+	public String getGroupType() {
+		return frmGroupType.getType();
+	}
 
-    public List<SensorModel> getSharedSensors() {
-        return frmShareSensors.getSharedSensors();
-    }
+	public List<SensorModel> getSharedSensors() {
+		return frmShareSensors.getSharedSensors();
+	}
 
-    public int getWizardState() {
-        return state;
-    }
+	public int getWizardState() {
+		return state;
+	}
 
-    private void initButtons() {
+	private void initButtons() {
 
-        btnNext = new Button("Next");
-        btnNext.setIconStyle("sense-btn-icon-go");
+		btnNext = new Button("Next");
+		btnNext.setIconStyle("sense-btn-icon-go");
 
-        btnBack = new Button("Back");
-        btnBack.setEnabled(false);
+		btnBack = new Button("Back");
+		btnBack.setEnabled(false);
 
-        btnCancel = new Button("Cancel");
+		btnCancel = new Button("Cancel");
 
-        addButton(btnBack);
-        addButton(btnNext);
-        addButton(btnCancel);
-    }
+		addButton(btnBack);
+		addButton(btnNext);
+		addButton(btnCancel);
+	}
 
-    public void setBusy(boolean busy) {
-        if (busy) {
-            btnNext.setIconStyle("sense-btn-icon-loading");
-            btnNext.disable();
-        } else {
-            btnNext.setIconStyle("sense-btn-icon-go");
-            btnNext.enable();
-        }
-    }
+	public void setBusy(boolean busy) {
+		if (busy) {
+			btnNext.setIconStyle("sense-btn-icon-loading");
+			btnNext.disable();
+		} else {
+			btnNext.setIconStyle("sense-btn-icon-go");
+			btnNext.enable();
+		}
+	}
 
-    public void setMemberRights(boolean createMembers, boolean createSensors, boolean readMembers,
-            boolean readSensors, boolean deleteMembers, boolean deleteSensors) {
-        frmMemberRights.setMemberRights(createMembers, createSensors, readMembers, readSensors,
-                deleteMembers, deleteSensors);
-    }
+	public void setMemberRights(boolean createMembers, boolean createSensors, boolean readMembers,
+			boolean readSensors, boolean deleteMembers, boolean deleteSensors) {
+		frmMemberRights.setMemberRights(createMembers, createSensors, readMembers, readSensors,
+				deleteMembers, deleteSensors);
+	}
 
-    public void setReqSensors(List<String> sensorNames) {
-        frmShareSensors.setReqSensors(sensorNames);
-    }
+	public void setReqSensors(List<String> sensorNames) {
+		frmShareSensors.setReqSensors(sensorNames);
+	}
 
-    public void setWizardState(int state) {
-        this.state = state;
+	public void setWizardState(int state) {
+		this.state = state;
 
-        switch (state) {
-            case States.GROUP_NAME :
-                showGroupNameForm();
-                break;
-            case States.GROUP_TYPE_CHOICE :
-                showGroupTypeChoice();
-                break;
-            case States.ALL_VISIBLE_GROUPS :
-                showAllGroupsList();
-                break;
-            case States.SHARE_SENSORS :
-                showShareSensors();
-                break;
-            case States.MEMBER_RIGHTS :
-                showMemberRights();
-                break;
-            default :
-                LOG.warning("Unexpected new state: " + state);
-        }
-    }
+		switch (state) {
+		case States.GROUP_NAME:
+			showGroupNameForm();
+			break;
+		case States.GROUP_TYPE_CHOICE:
+			showGroupTypeChoice();
+			break;
+		case States.ALL_VISIBLE_GROUPS:
+			showAllGroupsList();
+			break;
+		case States.SHARE_SENSORS:
+			showShareSensors();
+			break;
+		case States.MEMBER_RIGHTS:
+			showMemberRights();
+			break;
+		default:
+			LOG.warning("Unexpected new state: " + state);
+		}
+	}
 
-    private void showAllGroupsList() {
-        // update active item
-        layout.setActiveItem(frmAllVisibleGroups);
+	private void showAllGroupsList() {
+		// update active item
+		layout.setActiveItem(frmAllVisibleGroups);
 
-        // update buttons
-        btnNext.setText("Next");
-        btnBack.setEnabled(true);
+		// update buttons
+		btnNext.setText("Next");
+		btnBack.setEnabled(true);
 
-        if (null != buttonBinding) {
-            buttonBinding.removeButton(btnNext);
-        }
-        buttonBinding = new FormButtonBinding(frmAllVisibleGroups);
-        buttonBinding.addButton(btnNext);
-    }
+		if (null != buttonBinding) {
+			buttonBinding.removeButton(btnNext);
+		}
+		buttonBinding = new FormButtonBinding(frmAllVisibleGroups);
+		buttonBinding.addButton(btnNext);
+	}
 
-    private void showGroupNameForm() {
-        // update active item
-        layout.setActiveItem(frmGroupName);
+	private void showGroupNameForm() {
+		// update active item
+		layout.setActiveItem(frmGroupName);
 
-        // update buttons
-        btnNext.setText("Next");
-        btnBack.setEnabled(true);
+		// update buttons
+		btnNext.setText("Next");
+		btnBack.setEnabled(true);
 
-        if (null != buttonBinding) {
-            buttonBinding.removeButton(btnNext);
-        }
-        buttonBinding = new FormButtonBinding(frmGroupName);
-        buttonBinding.addButton(btnNext);
-    }
+		if (null != buttonBinding) {
+			buttonBinding.removeButton(btnNext);
+		}
+		buttonBinding = new FormButtonBinding(frmGroupName);
+		buttonBinding.addButton(btnNext);
+	}
 
-    private void showGroupTypeChoice() {
-        // update active item
-        layout.setActiveItem(frmGroupType);
+	private void showGroupTypeChoice() {
+		// update active item
+		layout.setActiveItem(frmGroupType);
 
-        // update buttons
-        btnNext.setText("Next");
-        btnBack.setEnabled(false);
+		// update buttons
+		btnNext.setText("Next");
+		btnBack.setEnabled(false);
 
-        if (null != buttonBinding) {
-            buttonBinding.removeButton(btnNext);
-        }
-        buttonBinding = new FormButtonBinding(frmGroupType);
-        buttonBinding.addButton(btnNext);
-    }
+		if (null != buttonBinding) {
+			buttonBinding.removeButton(btnNext);
+		}
+		buttonBinding = new FormButtonBinding(frmGroupType);
+		buttonBinding.addButton(btnNext);
+	}
 
-    private void showMemberRights() {
-        // update active item
-        layout.setActiveItem(frmMemberRights);
+	private void showMemberRights() {
+		// update active item
+		layout.setActiveItem(frmMemberRights);
 
-        // update buttons
-        btnNext.setText("Finish");
-        btnBack.setEnabled(true);
+		// update buttons
+		btnNext.setText("Finish");
+		btnBack.setEnabled(true);
 
-        if (null != buttonBinding) {
-            buttonBinding.removeButton(btnNext);
-        }
-        buttonBinding = new FormButtonBinding(frmMemberRights);
-        buttonBinding.addButton(btnNext);
-    }
+		if (null != buttonBinding) {
+			buttonBinding.removeButton(btnNext);
+		}
+		buttonBinding = new FormButtonBinding(frmMemberRights);
+		buttonBinding.addButton(btnNext);
+	}
 
-    private void showShareSensors() {
-        // update active item
-        layout.setActiveItem(frmShareSensors);
+	private void showShareSensors() {
+		// update active item
+		layout.setActiveItem(frmShareSensors);
 
-        // update buttons
-        btnNext.setText("Next");
-        btnBack.setEnabled(true);
+		// update buttons
+		btnNext.setText("Next");
+		btnBack.setEnabled(true);
 
-        if (null != buttonBinding) {
-            buttonBinding.removeButton(btnNext);
-        }
-        buttonBinding = new FormButtonBinding(frmShareSensors);
-        buttonBinding.addButton(btnNext);
-    }
+		if (null != buttonBinding) {
+			buttonBinding.removeButton(btnNext);
+		}
+		buttonBinding = new FormButtonBinding(frmShareSensors);
+		buttonBinding.addButton(btnNext);
+	}
 }
--- src/nl/sense_os/commonsense/client/groups/join/components/ShareSensorsForm.java
+++ src/nl/sense_os/commonsense/client/groups/join/components/ShareSensorsForm.java
@@ -2,8 +2,6 @@ package nl.sense_os.commonsense.client.groups.join.components;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import nl.sense_os.commonsense.client.common.components.WizardFormPanel;
 import nl.sense_os.commonsense.client.common.models.SensorModel;
@@ -29,185 +27,180 @@ import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 
 public class ShareSensorsForm extends WizardFormPanel {
 
-    private static final Logger LOG = Logger.getLogger(ShareSensorsForm.class.getName());
+	private List<String> reqSensorNames;
+	private LabelField lblReqSensors;
 
-    private List<String> reqSensorNames;
-    private LabelField lblReqSensors;
+	private GroupingStore<SensorModel> store;
+	private Grid<SensorModel> grid;
+	private ToolBar filterBar;
 
-    private GroupingStore<SensorModel> store;
-    private Grid<SensorModel> grid;
-    private ToolBar filterBar;
+	public ShareSensorsForm(final List<String> reqSensors, List<String> optSensors,
+			List<SensorModel> sensors) {
 
-    public ShareSensorsForm(final List<String> reqSensors, List<String> optSensors,
-            List<SensorModel> sensors) {
-        super();
+		reqSensorNames = reqSensors;
 
-        LOG.setLevel(Level.ALL);
+		initGrid(sensors);
+		initFilters();
+		ContentPanel panel = new ContentPanel(new FitLayout());
+		panel.setHeaderVisible(false);
+		panel.setStyleAttribute("backgroundColor", "white");
+		panel.setTopComponent(filterBar);
+		panel.add(grid);
 
-        reqSensorNames = reqSensors;
+		AdapterField field = new AdapterField(panel) {
 
-        initGrid(sensors);
-        initFilters();
-        ContentPanel panel = new ContentPanel(new FitLayout());
-        panel.setHeaderVisible(false);
-        panel.setStyleAttribute("backgroundColor", "white");
-        panel.setTopComponent(filterBar);
-        panel.add(grid);
+			@Override
+			public boolean isValid() {
+				return isValid(false);
+			}
 
-        AdapterField field = new AdapterField(panel) {
+			@Override
+			public boolean isValid(boolean silent) {
 
-            @Override
-            public boolean isValid() {
-                return isValid(false);
-            }
+				selectRequiredSensors();
 
-            @Override
-            public boolean isValid(boolean silent) {
+				String missingSensors = "";
+				List<SensorModel> selected = grid.getSelectionModel().getSelectedItems();
+				for (String reqName : reqSensorNames) {
+					boolean reqSensorSelected = false;
+					for (SensorModel sensor : selected) {
+						if (sensor.getName().equals(reqName)) {
+							reqSensorSelected = true;
+							break;
+						}
+					}
+					if (!reqSensorSelected) {
+						missingSensors += reqName + ", ";
+					}
+				}
+				boolean valid = missingSensors.length() == 0;
 
-                selectRequiredSensors();
+				// display message
+				// TODO does not work
+				if (!valid && !silent) {
+					if (missingSensors.length() > 0) {
+						missingSensors = missingSensors.substring(0, missingSensors.length() - 2);
+					}
+					markInvalid("You need to select " + missingSensors + " sensor(s)");
+				}
+				return valid;
+			}
+		};
+		field.setHeight(325);
+		field.setResizeWidget(true);
+		field.setFieldLabel("Select the sensors that you want to share with the group");
+		add(field, new FormData(anchorSpec));
 
-                String missingSensors = "";
-                List<SensorModel> selected = grid.getSelectionModel().getSelectedItems();
-                for (String reqName : reqSensorNames) {
-                    boolean reqSensorSelected = false;
-                    for (SensorModel sensor : selected) {
-                        if (sensor.getName().equals(reqName)) {
-                            reqSensorSelected = true;
-                            break;
-                        }
-                    }
-                    if (!reqSensorSelected) {
-                        missingSensors += reqName + ", ";
-                    }
-                }
-                boolean valid = missingSensors.length() == 0;
+		lblReqSensors = new LabelField("foo");
+		lblReqSensors.setHideLabel(true);
+		setReqSensors(reqSensors);
+		add(lblReqSensors, new FormData("-5"));
+	}
 
-                // display message
-                // TODO does not work
-                if (!valid && !silent) {
-                    if (missingSensors.length() > 0) {
-                        missingSensors = missingSensors.substring(0, missingSensors.length() - 2);
-                    }
-                    markInvalid("You need to select " + missingSensors + " sensor(s)");
-                }
-                return valid;
-            }
-        };
-        field.setHeight(325);
-        field.setResizeWidget(true);
-        field.setFieldLabel("Select the sensors that you want to share with the group");
-        add(field, new FormData(anchorSpec));
+	public Grid<SensorModel> getGrid() {
+		return grid;
+	}
 
-        lblReqSensors = new LabelField("foo");
-        lblReqSensors.setHideLabel(true);
-        setReqSensors(reqSensors);
-        add(lblReqSensors, new FormData("-5"));
-    }
+	public List<SensorModel> getSharedSensors() {
+		return grid.getSelectionModel().getSelection();
+	}
 
-    public Grid<SensorModel> getGrid() {
-        return grid;
-    }
+	/**
+	 * Initializes filter toolbar for the grid with sensors. The bar contains text filter and an
+	 * owner filter.
+	 */
+	private void initFilters() {
 
-    public List<SensorModel> getSharedSensors() {
-        return grid.getSelectionModel().getSelection();
-    }
+		// text filter
+		SensorTextFilter<SensorModel> textFilter = new SensorTextFilter<SensorModel>();
+		textFilter.bind(store);
 
-    /**
-     * Initializes filter toolbar for the grid with sensors. The bar contains text filter and an
-     * owner filter.
-     */
-    private void initFilters() {
+		// add filters to filter bar
+		filterBar = new ToolBar();
+		filterBar.add(new LabelToolItem("Filter: "));
+		filterBar.add(textFilter);
+	}
 
-        // text filter
-        SensorTextFilter<SensorModel> textFilter = new SensorTextFilter<SensorModel>();
-        textFilter.bind(store);
+	private void initGrid(List<SensorModel> sensors) {
 
-        // add filters to filter bar
-        filterBar = new ToolBar();
-        filterBar.add(new LabelToolItem("Filter: "));
-        filterBar.add(textFilter);
-    }
+		// list store
+		store = new GroupingStore<SensorModel>();
+		store.setKeyProvider(new SenseKeyProvider<SensorModel>());
+		store.setMonitorChanges(true);
+		store.add(sensors);
 
-    private void initGrid(List<SensorModel> sensors) {
+		// selection model for the grid
+		CheckBoxSelectionModel<SensorModel> selectionModel = new CheckBoxSelectionModel<SensorModel>();
 
-        // list store
-        store = new GroupingStore<SensorModel>();
-        store.setKeyProvider(new SenseKeyProvider<SensorModel>());
-        store.setMonitorChanges(true);
-        store.add(sensors);
+		// Column model
+		List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
+		configs.add(selectionModel.getColumn());
+		configs.addAll(LibraryColumnsFactory.create().getColumns());
+		ColumnModel cm = new ColumnModel(configs);
 
-        // selection model for the grid
-        CheckBoxSelectionModel<SensorModel> selectionModel = new CheckBoxSelectionModel<SensorModel>();
+		// grouping view for the grid
+		GroupingView view = new GroupingView();
+		view.setShowGroupedColumn(true);
+		view.setForceFit(true);
+		view.setGroupRenderer(new SensorGroupRenderer(cm));
 
-        // Column model
-        List<ColumnConfig> configs = new ArrayList<ColumnConfig>();
-        configs.add(selectionModel.getColumn());
-        configs.addAll(LibraryColumnsFactory.create().getColumns());
-        ColumnModel cm = new ColumnModel(configs);
+		grid = new Grid<SensorModel>(store, cm);
+		grid.setModelProcessor(new SensorProcessor<SensorModel>());
+		grid.setView(view);
+		grid.setBorders(false);
+		grid.setId("group-sensor-share-grid");
+		grid.setStateful(true);
+		grid.setLoadMask(true);
+		grid.setSelectionModel(selectionModel);
+	}
 
-        // grouping view for the grid
-        GroupingView view = new GroupingView();
-        view.setShowGroupedColumn(true);
-        view.setForceFit(true);
-        view.setGroupRenderer(new SensorGroupRenderer(cm));
+	private void selectRequiredSensors() {
 
-        grid = new Grid<SensorModel>(store, cm);
-        grid.setModelProcessor(new SensorProcessor<SensorModel>());
-        grid.setView(view);
-        grid.setBorders(false);
-        grid.setId("group-sensor-share-grid");
-        grid.setStateful(true);
-        grid.setLoadMask(true);
-        grid.setSelectionModel(selectionModel);
-    }
+		List<SensorModel> sensors = store.getModels();
+		List<SensorModel> requiredSensors = new ArrayList<SensorModel>();
+		for (SensorModel sensor : sensors) {
+			boolean required = false;
+			for (String reqName : reqSensorNames) {
+				if (sensor.getName().equals(reqName)) {
+					required = true;
+					break;
+				}
+			}
+			if (required) {
+				requiredSensors.add(sensor);
+			}
+		}
 
-    private void selectRequiredSensors() {
+		// select the sensors in the grid
+		grid.getSelectionModel().select(true,
+				requiredSensors.toArray(new SensorModel[requiredSensors.size()]));
+	}
 
-        List<SensorModel> sensors = store.getModels();
-        List<SensorModel> requiredSensors = new ArrayList<SensorModel>();
-        for (SensorModel sensor : sensors) {
-            boolean required = false;
-            for (String reqName : reqSensorNames) {
-                if (sensor.getName().equals(reqName)) {
-                    required = true;
-                    break;
-                }
-            }
-            if (required) {
-                requiredSensors.add(sensor);
-            }
-        }
+	public void setReqSensors(List<String> sensorNames) {
 
-        // select the sensors in the grid
-        grid.getSelectionModel().select(true,
-                requiredSensors.toArray(new SensorModel[requiredSensors.size()]));
-    }
+		reqSensorNames = sensorNames;
 
-    public void setReqSensors(List<String> sensorNames) {
+		String labelTxt = "You are required to share the following sensors to join this group: ";
+		boolean visible = true;
+		if (sensorNames.size() > 1) {
+			for (String sensor : sensorNames) {
+				labelTxt += "'" + sensor + "', ";
+			}
+			labelTxt = labelTxt.substring(0, labelTxt.length() - 2);
+			visible = true;
 
-        reqSensorNames = sensorNames;
+		} else if (sensorNames.size() == 1) {
+			labelTxt = "You are required to share your '" + sensorNames.get(0) + "' sensor.";
+			visible = true;
 
-        String labelTxt = "You are required to share the following sensors to join this group: ";
-        boolean visible = true;
-        if (sensorNames.size() > 1) {
-            for (String sensor : sensorNames) {
-                labelTxt += "'" + sensor + "', ";
-            }
-            labelTxt = labelTxt.substring(0, labelTxt.length() - 2);
-            visible = true;
+		} else {
+			labelTxt = "This group does not require you to share certain sensors.";
+			visible = false;
+		}
 
-        } else if (sensorNames.size() == 1) {
-            labelTxt = "You are required to share your '" + sensorNames.get(0) + "' sensor.";
-            visible = true;
+		lblReqSensors.setText(labelTxt);
+		lblReqSensors.setVisible(visible);
 
-        } else {
-            labelTxt = "This group does not require you to share certain sensors.";
-            visible = false;
-        }
-
-        lblReqSensors.setText(labelTxt);
-        lblReqSensors.setVisible(visible);
-
-        selectRequiredSensors();
-    }
+		selectRequiredSensors();
+	}
 }
