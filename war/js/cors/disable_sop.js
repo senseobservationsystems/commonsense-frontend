524f4f97b27b674ac84665f606c9c4e48b5ed6d5
Steven Mulder <steven@sense-os.nl>
2012-08-07 16:25:29 +0200

    removed unused javascript: closure library, libphonenumber and cors tools

--- war/js/cors/disable_sop.js
+++
@@ -1,8 +0,0 @@
-if (navigator.userAgent.indexOf("Firefox") != -1) {
-    try {
-        netscape.security.PrivilegeManager.enablePrivilege("UniversalBrowserRead");
-    } 
-    catch (e) {
-        alert("Permission UniversalBrowserRead denied -- not running Mozilla?");
-    }
-}
