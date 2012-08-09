e5d19924c9047da51f1d8cc6cd2783b3f92aa480
Steven Mulder <steven@sense-os.nl>
2012-08-09 10:18:22 +0200

    made sense logo adapt to LIVE/RC/DEV mode

--- src/nl/sense_os/commonsense/login/client/main/component/LoginNavigationBar.java
+++ src/nl/sense_os/commonsense/login/client/main/component/LoginNavigationBar.java
@@ -1,8 +1,13 @@
 package nl.sense_os.commonsense.login.client.main.component;
 
+import nl.sense_os.commonsense.common.client.resource.CSResources;
+import nl.sense_os.commonsense.common.client.util.Constants;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 
 public class LoginNavigationBar extends Composite {
@@ -12,8 +17,16 @@ public class LoginNavigationBar extends Composite {
 	interface NavigationBarUiBinder extends UiBinder<Widget, LoginNavigationBar> {
 	}
 
+	@UiField
+	Image logo;
+
 	public LoginNavigationBar() {
 		initWidget(uiBinder.createAndBindUi(this));
-	}
 
+		if (Constants.RC_MODE) {
+			logo.setResource(CSResources.INSTANCE.logoTest());
+		} else if (Constants.DEV_MODE) {
+			logo.setResource(CSResources.INSTANCE.logoDev());
+		}
+	}
 }
