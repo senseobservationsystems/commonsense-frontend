3d1319100cfbe4d24ff995c8fc5d74ead2929c27
Steven Mulder <steven@sense-os.nl>
2013-05-08 12:12:34 +0200

    Updated welcome page

--- src/nl/sense_os/commonsense/main/client/viz/tabs/VizTabPanel.java
+++ src/nl/sense_os/commonsense/main/client/viz/tabs/VizTabPanel.java
@@ -1,14 +1,12 @@
 package nl.sense_os.commonsense.main.client.viz.tabs;
 
-import nl.sense_os.commonsense.main.client.ext.util.SenseIconProvider;
-
-import com.extjs.gxt.ui.client.util.IconHelper;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.TabItem;
 import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.layout.FitData;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.layout.LayoutData;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.Frame;
 
 /**
@@ -16,30 +14,30 @@ import com.google.gwt.user.client.ui.Frame;
  */
 public class VizTabPanel extends TabPanel {
 
-    private final TabItem tabItemWelcome;
+	private final TabItem tabItemWelcome;
 
-    public VizTabPanel() {
-        setId("viz-tab-panel");
-        setSize("100%", "100%");
-        setPlain(true);
-        setMinTabWidth(120);
-        setResizeTabs(true);
-        addStyleName("transparent");
+	public VizTabPanel() {
+		setId("viz-tab-panel");
+		setSize("100%", "100%");
+		setPlain(true);
+		setMinTabWidth(120);
+		setResizeTabs(true);
+		addStyleName("transparent");
 
-        // Welcome tab item
-        final Frame frameWelcome = new Frame("http://welcome.sense-os.nl/node/9");
-        frameWelcome.setStylePrimaryName("senseFrame");
+		// Welcome tab item
+		final Frame frameWelcome = new Frame(GWT.getModuleBaseForStaticFiles()
+				+ "/intro/intro.html");
+		frameWelcome.setStylePrimaryName("senseFrame");
 
-        tabItemWelcome = new TabItem("Welcome");
-        tabItemWelcome.setIcon(IconHelper.create(SenseIconProvider.SENSE_ICONS_PATH + "help.png"));
-        tabItemWelcome.setLayout(new FitLayout());
-        LayoutData data = new FitData(new Margins(0));
-        tabItemWelcome.add(frameWelcome, data);
+		tabItemWelcome = new TabItem("Welcome");
+		tabItemWelcome.setLayout(new FitLayout());
+		LayoutData data = new FitData(new Margins(0));
+		tabItemWelcome.add(frameWelcome, data);
 
-        add(tabItemWelcome);
-    }
+		add(tabItemWelcome);
+	}
 
-    public TabItem getTabItemWelcome() {
-        return tabItemWelcome;
-    }
+	public TabItem getTabItemWelcome() {
+		return tabItemWelcome;
+	}
 }
